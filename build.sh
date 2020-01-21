be0bd377b834060c4294c4623a722f4d5e6dd5bd
Rania Amina <reaamina@gmail.com>
2020-01-21 07:31:12 +0700

    move icons and update build script

--- build.sh
+++ build.sh
@@ -21,6 +21,24 @@ SVG_FOLDER=""
 # check PNG directory
 PNG_FOLDER=$(echo $SVG_FOLDER | sed "s/_svg//")
 [ -d "$PNG_FOLDER" ] && rm -rfv "$PNG_FOLDER"
+# Resizing SVG to all required files
+NUMSVG=$(find "$SVG_FOLDER" -name "sc_*.svg" | wc -l)
+echo "Preparing $NUMSVG SVGs ..."
+counter=1
+find "$SVG_FOLDER" -name "sc_*.svg" | while read i;
+do
+    CURFILE=`basename $i | cut -d '_' -f 2`
+    DIRFILE=`dirname $i`
+    # convert to normal size
+    rsvg-convert -f svg "$i" -w 32 -a -o "$DIRFILE/$CURFILE"
+    # convert to lc size
+    rsvg-convert -f svg "$i" -w 24 -a -o "$DIRFILE/lc_$CURFILE"
+    current=$((counter%100))
+    if [[ "$current" == "0" ]];then
+        echo "[$counter/$NUMSVG] ..."
+    fi
+    let counter++
+done
 
 # Export SVG to PNG 
 # prepairing for export
@@ -49,24 +67,24 @@ done
 sed -i "s/.svg/.png/g" "$PNG_FOLDER/links.txt" 
 # 4 - Create ZIP image for SVG and PNG
 echo "Creating ZIP Images"
-pushd "$SVG_FOLDER"
+pushd "$SVG_FOLDER" > /dev/null
 zip -q -r -D ../build/"$SVG_FOLDER".zip *
-popd
-pushd "$PNG_FOLDER"
+popd > /dev/null
+pushd "$PNG_FOLDER" > /dev/null
 zip -q -r -D ../build/"$PNG_FOLDER".zip *
-popd
+popd > /dev/null
 # 5 - Copying ZIP to OXT env directory
 THEMENAME=$(echo $PNG_FOLDER | sed "s/images_//")
 cp build/"$PNG_FOLDER".zip build/"$THEMENAME"-IconSet/iconsets/
 cp build/"$SVG_FOLDER".zip build/"$THEMENAME"-SVG-IconSet/iconsets/
 # 6 - Create OXT files
 echo "Creating Extension Files"
-pushd build/"$THEMENAME"-IconSet/
+pushd build/"$THEMENAME"-IconSet/ > /dev/null
 zip -q -r -D ../"$THEMENAME"-IconSet.oxt *
-popd
-pushd build/"$THEMENAME"-SVG-IconSet/
+popd > /dev/null
+pushd build/"$THEMENAME"-SVG-IconSet/ > /dev/null
 zip -q -r -D ../"$THEMENAME"-SVG-IconSet.oxt *
-popd
+popd > /dev/null
 echo "Path of Generated oxt files:"
 echo "=> 1. $PWD/build/$THEMENAME-IconSet.oxt"
 echo "=> 2. $PWD/build/$THEMENAME-SVG-IconSet.oxt"
